88b00047d692c7b1665fbdab2dcdf2edab34ab89
chrliedean <64105532+chrliedean@users.noreply.github.com>
2022-06-18 12:15:57 -0400

    updated code

--- components/GuessInput.js
+++ components/GuessInput.js
@@ -20,7 +20,7 @@ export default function GuessInput() {
         //Search celebrity on Wikidata
         const wdurl = wdk.cirrusSearchPages({ search: name , haswbstatement: ['P31=Q5','-P570']})
         console.log(wdurl)
-        wdurl = "https://cors-anywhere.herokuapp.com/" + wdurl
+        //wdurl = "https://cors-anywhere.herokuapp.com/" + wdurl
         let wdresponse = await fetch(wdurl)
             .then(res => res.json())
             .then(wdk.parse.wb.pagesTitles) // what on earth does this do
